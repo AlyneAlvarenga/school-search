f03271c2c99c7d280adfdd15c6680f09abb5ed90
AlyneAlvarenga <alynevs@gmail.com>
2020-05-22 16:53:43 -0400

    can generate pdf and pass props

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import SchoolData from './SchoolData';
 import './App.css';
+import { PDFViewer } from '@react-pdf/renderer';
+import PDFDoc from './PDFDoc';
 
 const App = () => {
   // useEffect(() => {
@@ -70,6 +72,9 @@ const App = () => {
             </>
           : null
       }
+      <PDFViewer width={500} height={800}>
+        <PDFDoc school={currentSchool}/>
+      </PDFViewer>
     </div>
   );
 }
--- routes/api/grades.js
+++ routes/api/grades.js
@@ -7,6 +7,7 @@ router.get('/:school/grades', (req, res) => {
   Student.aggregate([
     { $match: { schoolName: req.params.school }},
     { $unwind: "$grades" },
+  // gives me eight objects, where "grades" now holds an object with a single subject and grade. so Logan appears in two objects, one with "grades": "science" and another with "grades": "math"
     { $group: { _id: "$grades.subject", grades: {"$push": "$grades.grade"} } }
   ]).then(objs => {
     res.json(objs)
